4ae5d080d864c9938b7cbf24f071df81442c5e1f
Scot <bscot.hagen@gmail.com>
2021-07-29 12:30:30 -0700

    Added a controller and a View

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,17 +8,32 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        //public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            //return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        //public string Welcome(string name, int iD = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {iD}");
+        //}
+
+        public IActionResult Welcome(string name, int numTimes)
         {
-            return "This is the Welcome action method...";
+            ViewData["Message"] = "Hello" + name;
+            ViewData["Numtimes"] = numTimes;
+
+            return View();
         }
     }
 }
